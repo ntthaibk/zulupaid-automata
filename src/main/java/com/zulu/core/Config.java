6dede69dad56b19806823f9b54a018540d96d2a1
Thai Nguyen <62538035+ntthaibk@users.noreply.github.com>
2021-07-06 19:37:49 +0700

    update: Some technique

---
+++ src/main/java/com/zulu/core/Config.java
@@ -0,0 +1,12 @@
+package com.zulu.core;
+
+import lombok.Data;
+
+@Data
+public class Config {
+    private String name;
+
+    
+
+
+}
--- src/test/java/ZuluDebugger.java
+++ src/test/java/ZuluDebugger.java
@@ -1,3 +1,32 @@
+import com.zulu.core.Config;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class ZuluDebugger {
+
+
+    @Test
+    public void doSomething(){
+        Config config = new Config();
+
+        config.setName("ZuluPaid");
+        String zoneid = config.getName();
+        System.out.println(zoneid);
+    }
+
+    @Test
+    public void testMinhFunction(){
+        String[] days = new String[]{"Mon","Tue","Wed","Thu","Fri", "Sat", "Sun"};
+        int deliveredDay = getDeliverDay(Arrays.asList(days).indexOf("Wed"),4);
+        System.out.println(days[deliveredDay]);
+    }
+
+
+    public int getDeliverDay(int dateNo, int workdingDays){
+        return (dateNo+workdingDays)%7;
+
+    }
 
 }
